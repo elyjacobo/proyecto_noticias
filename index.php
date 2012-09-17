f22ae5600f5792e57d11c6036ca4ce85ae82b730
elyjacobo <rivera.jacobito@gmail.com>
2012-09-17 07:10:41 -0600

    agregando el index.php de administracion

---
+++ index.php
@@ -0,0 +1,39 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/back.css" type="text/css" rel="stylesheet">
+        <title></title>
+    </head>
+    <body>
+        <div id="contenedor">
+            <div id="header">
+                <img src="img/banner2.png">                
+            </div>
+            
+            <div id="menu">
+                <ul id="button">  
+                    <li><a href=”#”>Nueva noticia</a></li>  
+                    <li><a href=”#”>Actualizar</a></li>  
+                    <li><a href=”#”>Borrar</a></li>  
+                    <li><a href=”#”>Ver lista</a></li> 
+                </ul>
+            </div>
+            
+            <div id="cuerpo">
+            </div>
+            
+            <div id="footer">
+                WebMaster: elyjacobo<br>
+                Ultima modificacion: 15 de septiembre de 2012
+            </div>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
