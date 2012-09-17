d2868fff462ab46c4d2f9198b1adc7664645e383
elyjacobo <rivera.jacobito@gmail.com>
2012-09-17 08:52:25 -0600

    agregando la pagina para eliminar noticias

---
+++ eliminar.php
@@ -0,0 +1,22 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Eliminar noticia</title>
+    </head>
+    <body>
+        <h1>Eliminar noticia</h1>
+        <b>Titulo:</b><select name="Eliminar">
+            <option value="eliminar">-Selecione la noticia a eliminar-</option>
+        </select>
+        <br>
+        <input type="submit" value="Eliminar">
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
