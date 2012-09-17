610f16f8ec3379681271f5d1a7072eb0c5993619
elyjacobo <rivera.jacobito@gmail.com>
2012-09-16 23:39:32 -0600

    añadiendo el login

---
+++ login.php
@@ -0,0 +1,37 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Administracion de noticias</title>
+    </head>
+    <body>
+        <form action="#" method="post">
+            <h1>Bienvenido al sistema</h1>
+            <table cellspacing="1" cellpadding="2" border="0">
+                        <tbody>
+                            <tr>
+                                <td align="right"> Usuario:</td>
+                                <td><input type="text" name="username" size="25" maxlength="40" /></td>
+                            </tr>
+                            <tr>
+                                <td align="right">Contraseña:</td>
+                                <td><input type="password" name="password" size="25" maxlength="32" /></td>
+                            </tr>
+                            <tr align="center">
+                                <td colspan="2">
+                                    <input type="submit" class="mainoption" name="login" value="Entrar" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+               </form>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
